45b39acb94905bba256175d67a75501e2c356ab1
olyatjtaylo <162665321+olyatjtaylo@users.noreply.github.com>
2024-04-29 01:34:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("Welcome to Bob's Multi""model""Verse")
+st.caption("🚀 A streamlit powered by OpenAI LLM and other tools... ")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "What's good in the hood, my clippa?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
